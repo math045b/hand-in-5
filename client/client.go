df3bf7ffaf4ba229efe7ce6b0a38635aaa11e4a8
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-16 23:01:06 +0100

    Semi working solution

---
+++ client/client.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	pb "hand-in-5/proto"
+	"os"
+	"strconv"
+	"strings"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	clientId := os.Args[1]
+	stringPort := os.Args[2]
+	port, _ := strconv.Atoi(stringPort)
+
+	node := &Client{
+		ID:        clientId,
+		nextPort:  port,
+		timestamp: 0,
+	}
+
+	node.StartClient()
+
+	reader := bufio.NewReader(os.Stdin)
+	println("You can input the following: ")
+	println("Bid {amount}")
+	println("Result")
+
+	lastbet := 0
+
+	for {
+
+		text, _ := reader.ReadString('\n')
+		splittext := strings.Split(text, " ")
+
+		if len(splittext) < 2 {
+			if strings.TrimSpace(splittext[0]) == "Result" {
+				println("Getting acution result")
+				req := &pb.ResultRequest{}
+				res, err := node.client.AuctionResult(context.Background(), req)
+				if err != nil {
+					println("Error getting auction result")
+				}
+				println(res.Response)
+			}
+		} else {
+			if splittext[0] == "Bid" {
+				num, _ := strconv.Atoi(strings.TrimSpace(splittext[1]))
+				if num < lastbet {
+					println("bet must be larger than last bet!")
+					continue
+				}
+				lastbet = num
+				bidReg := &pb.BidRequest{
+					Amount: int64(num),
+					Port:   clientId,
+				}
+				bidResponse, err := node.client.PlaceBid(context.Background(), bidReg)
+				if err != nil {
+					println("port was unresponsive incrementing port num")
+					node.IncrementPort()
+
+					continue
+				}
+				println(bidResponse.Response)
+			}
+		}
+	}
+}
+
+func (n *Client) IncrementPort() {
+	n.StartClient()
+}
+
+type Client struct {
+	client    pb.ServiceClient
+	ID        string
+	nextPort  int
+	timestamp int64
+}
+
+func (n *Client) StartClient() {
+	address := "localhost:" + strconv.Itoa(n.nextPort)
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Errorf("failed to connect to node %s: %v", n.ID, err)
+	}
+
+	n.client = pb.NewServiceClient(conn)
+	n.nextPort++
+}
--- server_node.go
+++ server_node.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"hand-in-5/proto"
+	"log"
 	"net"
 	"os"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -14,30 +17,150 @@ type ServerNode struct {
 	proto.UnimplementedServiceServer
 	port     string
 	nextNode proto.ServiceClient
+	action   map[string]int64
 }
+
+var isRunning bool
+var startTime time.Time
+var node *ServerNode
+var winner string
 
 func main() {
 	// starting node
-	clientPort := os.Args[1]
 
-	node := &ServerNode{
+	fmt.Printf("Starting server node with port %s and client port %s\n", os.Args[1], os.Args[2])
+	clientPort := os.Args[1]
+	isRunning = false
+	winner = ""
+	node = &ServerNode{
 		port: clientPort,
 	}
-
-	node.StartServer()
-
-	if len(os.Args) > 3 {
-
+	if len(os.Args) > 2 {
+		fmt.Printf("Server port: %s, clientport: %s \n", os.Args[1], os.Args[2])
+		node.StartClient(os.Args[2])
 	}
-
+	go RunAuction()
+	node.StartServer()
 }
 
-func (n *ServerNode) StartClient(port string) {
-	conn, err := grpc.NewClient("localhost:"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+func RunAuction() {
+	for {
+		if isRunning {
+			if time.Since(startTime) > (30 * time.Second) {
+				fmt.Println("start: ", startTime.String())
+				log.Println("The auction was ended")
+				isRunning = false
+				winner = AssertWinner()
+				node.nextNode.UpdateNodes(
+					context.Background(),
+					&proto.NodeUpdate{
+						Auctionstate:   node.action,
+						AuctionOngoing: isRunning,
+						Time:           startTime.Unix(),
+					})
+				break
+			}
+		}
+	}
+}
+
+func AssertWinner() (max string) {
+	maxVal := int64(0)
+	maxKey := ""
+	for key, value := range node.action {
+		if value > maxVal {
+			maxVal = value
+			maxKey = key
+		}
+	}
+	return maxKey
+}
+
+func (n *ServerNode) UpdateNodes(ctx context.Context, data *proto.NodeUpdate) (*proto.Empty, error) {
+	fmt.Printf("Port: %s Received update\n", n.port)
+	// updating node auction map
+	n.action = data.Auctionstate
+	isRunning = data.AuctionOngoing
+	startTime = time.Unix(data.Time, 0)
+
+	winner = AssertWinner()
+
+	state := &proto.NodeUpdate{
+		Auctionstate:   n.action,
+		AuctionOngoing: isRunning,
+		Time:           data.Time,
+	}
+
+	if n.nextNode != nil {
+		n.nextNode.UpdateNodes(context.Background(), state)
+	}
+
+	return &proto.Empty{}, nil
+}
+
+func (n *ServerNode) startTimer(t time.Time) {
+	log.Println("The auction was started")
+	startTime = t
+	update := &proto.NodeUpdate{
+		Auctionstate:   n.action,
+		AuctionOngoing: isRunning,
+		Time:           t.Unix(),
+	}
+	isRunning = true
+	n.nextNode.UpdateNodes(context.Background(), update)
+}
+
+func (n *ServerNode) PlaceBid(ctx context.Context, request *proto.BidRequest) (*proto.BidResponse, error) {
+	var response *proto.BidResponse
+	if n.action == nil {
+		n.startTimer(time.Now())
+		n.action = make(map[string]int64)
+		n.action[request.Port] = request.Amount
+		response = &proto.BidResponse{Response: fmt.Sprintf("%s: You have joined the auction with your bid of %d", request.Port, request.Amount)}
+	} else if n.action != nil && !isRunning {
+		response = &proto.BidResponse{Response: "The auction has ended"}
+	} else {
+		n.action[request.Port] = request.Amount
+		response = &proto.BidResponse{Response: fmt.Sprintf("Bid Received! New Bid: %d", n.action[request.Port])}
+		update := &proto.NodeUpdate{Auctionstate: n.action, AuctionOngoing: isRunning, Time: startTime.Unix()}
+		if n.nextNode != nil {
+			_, err := n.nextNode.UpdateNodes(context.Background(), update)
+			if err != nil {
+				fmt.Printf("Failed to forward update to next node: %v\n", err)
+			}
+		} else {
+			fmt.Printf("Port: %s - No next node to forward update to\n", n.port)
+		}
+	}
+
+	return response, nil
+}
+
+func (n *ServerNode) AuctionResult(context context.Context, request *proto.ResultRequest) (*proto.ResultResponse, error) {
+	log.Println("Auction result was requested")
+	winner = AssertWinner()
+	var response *proto.ResultResponse
+	if !isRunning && winner != "" {
+		response = &proto.ResultResponse{Response: fmt.Sprintf("Auction is over the winner was %s", winner)}
+	} else {
+		response = &proto.ResultResponse{Response: fmt.Sprintf("Auction is ongoing the current max bid is: %d", node.action[winner])}
+	}
+
+	return response, nil
+}
+
+func (n *ServerNode) StartClient(port string) error {
+	println("client is starting brrr")
+	address := "localhost:" + port
+	fmt.Printf("%s is trying to create a client on", n.port)
+	fmt.Println(address)
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		fmt.Println("could not make client")
+		return fmt.Errorf("failed to connect to next node on port %s: %w", port, err)
 	}
 	n.nextNode = proto.NewServiceClient(conn)
+	fmt.Printf("Port: %s - Connected to next node on port %s\n", n.port, port)
+	return nil
 }
 
 func (n *ServerNode) StartServer() {
